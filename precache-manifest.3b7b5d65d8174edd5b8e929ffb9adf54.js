54d63b2c142a9d22415cfde1fe7f5f9e40328bcb
mdazad1508 <mdazad15081999@gmail.com>
2022-08-16 20:48:18 +0530

    Updates

---
+++ precache-manifest.3b7b5d65d8174edd5b8e929ffb9adf54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d33925a197eb8705cbea69720694654",
+    "url": "/MoviesApp/index.html"
+  },
+  {
+    "revision": "7be10d8bbeafd664c51a",
+    "url": "/MoviesApp/static/css/main.1da99822.chunk.css"
+  },
+  {
+    "revision": "22daeca5b02b55968147",
+    "url": "/MoviesApp/static/js/2.42d1fe8e.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/MoviesApp/static/js/2.42d1fe8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7be10d8bbeafd664c51a",
+    "url": "/MoviesApp/static/js/main.8dab89af.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/MoviesApp/static/js/main.8dab89af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "079f9a408cd78c3bd783",
+    "url": "/MoviesApp/static/js/runtime-main.a48d401e.js"
+  }
+]);
